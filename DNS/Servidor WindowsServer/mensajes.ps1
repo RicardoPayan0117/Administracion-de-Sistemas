00c0ba391474a6a6ec2aebd323fe05716dab8412
RicardoPayan0117 <112823481+RicardoPayan0117@users.noreply.github.com>
2025-03-02 19:26:44 -0700

    Update mensajes.ps1

--- DNS/Servidor WindowsServer/mensajes.ps1
+++ DNS/Servidor WindowsServer/mensajes.ps1
@@ -1,25 +1,25 @@
 # mensajes.ps1
 
 function MensajeInicio {
-    Write-Host "¡Bienvenido al script de configuración del servidor DNS en Windows Server 2025!"
+    Write-Host "Bienvenido al script de configuracion del servidor DNS en Windows Server 2025"
 }
 
 function MensajeExito {
-    Write-Host "[✅] Servidor DNS configurado correctamente en Windows Server 2025."
+    Write-Host "Servidor DNS configurado correctamente en Windows Server 2025."
 }
 
 function MensajeError {
-    Write-Host "[❌] Hubo un error durante la configuración. Por favor, revisa los logs."
+    Write-Host "Hubo un error durante la configuracion. Por favor, revisa los logs."
 }
 
 function MensajeSubredInvalida {
-    Write-Host "[❌] Error: La subred ingresada no es válida. Debe estar en formato CIDR (ej. 192.168.1.0/24)."
+    Write-Host "Error: La subred ingresada no es valida. Debe estar en formato CIDR (ej. 192.168.1.0/24)."
 }
 
 function MensajeIpInvalida {
-    Write-Host "[❌] Error: La dirección IP ingresada no es válida."
+    Write-Host "Error: La direccion IP ingresada no es valida."
 }
 
 function MensajeDominioInvalido {
-    Write-Host "[❌] Error: El nombre del dominio ingresado no es válido."
+    Write-Host "Error: El nombre del dominio ingresado no es valido."
 }
