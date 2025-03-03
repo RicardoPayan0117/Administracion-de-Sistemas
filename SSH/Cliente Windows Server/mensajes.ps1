1a36d9f5362dd069bc941afd068908f28b8ed8d9
RicardoPayan0117 <112823481+RicardoPayan0117@users.noreply.github.com>
2025-03-02 19:25:38 -0700

    Update mensajes.ps1

--- SSH/Cliente Windows Server/mensajes.ps1
+++ SSH/Cliente Windows Server/mensajes.ps1
@@ -1,13 +1,13 @@
-# mensajes.ps1
+o# mensajes.ps1
 
 function MensajeInicio {
-    Write-Host "¡Bienvenido al script de configuración del cliente SSH en Windows Server 2025!"
+    Write-Host "Bienvenido al script de configuracion del cliente SSH en Windows Server 2025"
 }
 
 function MensajeExito {
-    Write-Host "¡Cliente SSH configurado y listo para usar!"
+    Write-Host "Cliente SSH configurado y listo para usar"
 }
 
 function MensajeError {
-    Write-Host "Hubo un error durante la configuración. Por favor, revisa los logs."
+    Write-Host "Hubo un error durante la configuracion. Por favor, revisa los logs."
 }
