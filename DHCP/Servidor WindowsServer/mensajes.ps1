c6e083f33651fe543460922df1cf1ac3f8deaa28
RicardoPayan0117 <112823481+RicardoPayan0117@users.noreply.github.com>
2025-03-02 19:28:18 -0700

    Update mensajes.ps1

--- DHCP/Servidor WindowsServer/mensajes.ps1
+++ DHCP/Servidor WindowsServer/mensajes.ps1
@@ -1,29 +1,29 @@
 # mensajes.ps1
 
 function MensajeInicio {
-    Write-Host "¡Bienvenido al script de configuración del servidor DHCP en Windows Server 2025!"
+    Write-Host "Bienvenido al script de configuracion del servidor DHCP en Windows Server 2025!"
 }
 
 function MensajeExito {
-    Write-Host "[✅] Servidor DHCP configurado correctamente y en ejecución." -ForegroundColor Green
+    Write-Host "Servidor DHCP configurado correctamente y en ejecucion." -ForegroundColor Green
 }
 
 function MensajeError {
-    Write-Host "[❌] Hubo un error durante la configuración. Por favor, revisa los logs." -ForegroundColor Red
+    Write-Host "Hubo un error durante la configuracion. Por favor, revisa los logs." -ForegroundColor Red
 }
 
 function MensajeSubredInvalida {
-    Write-Host "[❌] Error: La dirección de red ingresada no es válida. Debe estar en formato CIDR (ej. 192.168.1.0/24)." -ForegroundColor Red
+    Write-Host "Error: La direccion de red ingresada no es valida. Debe estar en formato CIDR (ej. 192.168.1.0/24)." -ForegroundColor Red
 }
 
 function MensajeIpInvalida {
-    Write-Host "[❌] Error: La dirección IP ingresada no es válida." -ForegroundColor Red
+    Write-Host "Error: La direccion IP ingresada no es valida." -ForegroundColor Red
 }
 
 function MensajeMascaraInvalida {
-    Write-Host "[❌] Error: La máscara de subred ingresada no es válida." -ForegroundColor Red
+    Write-Host "Error: La mascara de subred ingresada no es valida." -ForegroundColor Red
 }
 
 function MensajeDuracionInvalida {
-    Write-Host "[❌] Error: La duración de la concesión debe ser un número entero." -ForegroundColor Red
+    Write-Host "Error: La duración de la concesion debe ser un numero entero." -ForegroundColor Red
 }
